33dc70d261d728195d54db50d69bf821673227c6
Victor Arias <arias.sp@gmail.com>
2013-10-20 12:46:38 -0200

    fist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuctionAim::Application.config.secret_key_base = '445e606ea49435326f202a56bf1689aa1e03a52817e716f7bd192a5e49da19107dfed9d7c8f5555246dcbd6b04acae89c6bab3d62c052bfdb15359871fdafb98'
