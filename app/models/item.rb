c776bbd8d3b03ab7a23be60536af8899d13f3ae6
Victor Arias <arias.sp@gmail.com>
2013-10-20 19:10:58 -0200

    importing items from hashes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ActiveRecord::Base
   has_many :bids
 
-  validates_presence_of :title, :reserve_met, :ends_at, :published_at, :watched
+  validates_presence_of :title, :ends_at, :published_at
+
+  class << self
+    def import(raw)
+      find_or_create_by!(raw)
+    end
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,9 +4,33 @@ require 'spec_helper'
 describe Item do
   describe "validations" do
     it { should validate_presence_of :title }
-    it { should validate_presence_of :reserve_met }
     it { should validate_presence_of :ends_at }
     it { should validate_presence_of :published_at }
-    it { should validate_presence_of :watched }
+  end
+
+  describe "importation" do
+    let(:raw) do
+      {
+        id: 1,
+        title: "title",
+        reserve_met: false,
+        ends_at: 1.day.from_now,
+        published_at: 1.day.ago,
+        watched: false
+      }
+    end
+
+    it "import a raw item" do
+      expect do
+        Item.import raw
+      end.to change(Item, :count).by 1
+    end
+
+    it "does not duplicate items" do
+      expect do
+        Item.import raw
+        Item.import raw
+      end.to change(Item, :count).by 1
+    end
   end
 end
