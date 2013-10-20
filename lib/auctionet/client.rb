6b30828ac02e4725692b131e405677bfd23b6a85
Victor Arias <arias.sp@gmail.com>
2013-10-20 16:15:11 -0200

    fetching single items from auctionet

--- lib/auctionet/client.rb
+++ lib/auctionet/client.rb
@@ -3,17 +3,23 @@ require 'open-uri'
 module Auctionet; end
 class Auctionet::Client
   def fetch
-    parse perform_request
+    data = JSON.parse perform_request
+    data["items"]
+  end
+
+  def fetch_item id
+    data = JSON.parse perform_request id
+    data["item"]
   end
 
   private
 
-  def parse raw
-    data = JSON.parse raw
-    data["items"]
-  end
-
-  def perform_request
-    open("https://auctionet.com/api/v2/items.json").read
+  def perform_request(param = nil)
+    suffix = if param
+               "items/#{param}.json"
+             else
+               "items.json"
+             end
+    open("https://auctionet.com/api/v2/" + suffix).read
   end
 end
---
+++ spec/lib/auctionet_client_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require 'auctionet/client'
+
+describe Auctionet::Client do
+  describe "#fetch" do
+    before(:each) do
+      sample = File.read "spec/assets/items.json"
+      allow(subject).to receive(:perform_request).and_return sample
+    end
+
+    it "fetch items" do
+      expect(subject.fetch.size).to be > 0
+    end
+
+    it "returns an array" do
+      expect(subject.fetch).to be_kind_of(Array)
+    end
+  end
+
+  describe "#fetch_item" do
+    before(:each) do
+      sample = File.read "spec/assets/item.json"
+      allow(subject).to receive(:perform_request).and_return sample
+    end
+
+    it "fetch a single item" do
+      expect(subject.fetch_item(1)).to_not be_nil
+    end
+
+    it "returns the right item" do
+      item = subject.fetch_item(1)
+      expect(item["id"]).to be 100529
+    end
+  end
+end
