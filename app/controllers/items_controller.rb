496d03ed96a8be5463f0663f66728c8b187dfde0
Victor Arias <arias.sp@gmail.com>
2013-10-21 01:22:19 -0200

    refactoring root controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,18 +5,20 @@ class ItemsController < ApplicationController
   end
 
   def watch
-    @item = Item.find(params[:id])
-    @item.watch
-    @item.save!
-
+    change_watched_state :watch
     redirect_to root_path
   end
 
   def unwatch
-    @item = Item.find(params[:id])
-    @item.unwatch
-    @item.save!
-
+    change_watched_state :unwatch
     redirect_to root_path
+  end
+
+  private
+
+  def change_watched_state state
+    @item = Item.find(params[:id])
+    @item.public_send state
+    @item.save!
   end
 end
